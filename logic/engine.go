11e7b438be8582b06de43bceb7ac2a87435c77c8
Seyhak <seyhakly@gmail.com>
2024-04-21 10:10:59 +0200

    feat: handle showing results of voting

--- logic/engine.go
+++ logic/engine.go
@@ -6,13 +6,33 @@ import (
 	logic_types "github.com/seyhak/parliament/logic/types"
 )
 
-func checkResult() {
+func presentResult() {
 	fmt.Println("---result---")
 	fmt.Println(GetGlobalState().ProblemState.Votes)
+	state := GetGlobalState()
+	for key, votes := range state.ProblemState.Votes {
+		currentProblem := state.ProblemState.HistoryProblems[key]
+		fmt.Printf("Answers for %s \n", currentProblem.Title)
+		for idx, vote := range votes {
+			if len(currentProblem.Answers) <= idx {
+				break
+			}
+			fmt.Printf("Votes for: \"%s\" - %v\n", currentProblem.Answers[idx], vote)
+		}
+		fmt.Println("")
+	}
+}
+func checkResult() int {
+	// TODO
+	return 0
+
+}
+func HandleResult() {
+	presentResult()
+	checkResult()
 }
 
 func RunParliament(users *[]logic_types.User) {
 	handleProblems()
-	// TODO
-	checkResult()
+	HandleResult()
 }
--- logic/state_test.go
+++ logic/state_test.go
@@ -42,6 +42,57 @@ func TestUpdateAnswerState(t *testing.T) {
 	})
 }
 
+func TestAddProblemToHistory(t *testing.T) {
+	t.Run("Test problem is copied", func(t *testing.T) {
+		InitiateState(nil)
+		state := GetGlobalState()
+		answers := []string{"Ans 1", "Ans2"}
+		problem := Problem{"aa", "title", "some content", answers}
+		state.addProblemToHistory(problem)
+		historyProblem := state.ProblemState.HistoryProblems["aa"]
+		if historyProblem.Content != problem.Content {
+			t.Error("History not copied properly")
+		}
+		if historyProblem.Answers[0] != problem.Answers[0] {
+			t.Error("History not copied properly")
+		}
+
+		historyProblem.Answers[0] = "new value 123"
+		historyProblem.Content = "new Content 315"
+
+		if historyProblem.Content == problem.Content {
+			t.Error("History not copied properly")
+		}
+		if historyProblem.Answers[0] == problem.Answers[0] {
+			t.Error("History not copied properly")
+		}
+	})
+	t.Run("Add problem to history in state", func(t *testing.T) {
+		InitiateState(nil)
+		state := GetGlobalState()
+		answers := []string{"Ans 1", "Ans2"}
+		problem := Problem{"aa", "title", "some content", answers}
+		state.addProblemToHistory(problem)
+		if len(state.ProblemState.HistoryProblems) != 1 {
+			t.Error("Failed to add problem to history")
+		}
+		if state.ProblemState.HistoryProblems["aa"].Id != problem.Id {
+			t.Error("Adding not proper id")
+		}
+		if state.ProblemState.HistoryProblems["aa"].Title != problem.Title {
+			t.Error("Adding not proper Title")
+		}
+		if state.ProblemState.HistoryProblems["aa"].Content != problem.Content {
+			t.Error("Adding not proper Content")
+		}
+		for idx, val := range answers {
+			if state.ProblemState.HistoryProblems["aa"].Answers[idx] != val {
+				t.Error("Adding not proper Answers")
+			}
+		}
+	})
+}
+
 func TestMain(m *testing.M) {
 	InitiateState(nil)
 	m.Run()
